1f860a4f64e15c03d06b43d4e3de49252133fa32
M.Tayyeb <tayyeb0750@gmail.com>
2025-10-02 18:34:26 +0400

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import App from "./App.jsx";
 import About from "./routes/About.jsx";
 import Projects from "./routes/Projects.jsx";
 import Blogs from "./routes/Blogs.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  { path: "/m.tayyeb-portfolio", element: <App /> },
-  { path: "/m.tayyeb-portfolio/about", element: <About /> },
-  { path: "/m.tayyeb-portfolio/projects", element: <Projects /> },
-  { path: "/m.tayyeb-portfolio/blogs", element: <Blogs /> },
+const router = createHashRouter([
+  { path: "/", element: <App /> },
+  { path: "/about", element: <About /> },
+  { path: "/projects", element: <Projects /> },
+  { path: "/blogs", element: <Blogs /> },
 ]);
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
